15f00e7896136c6827e481963f5a3ea06104f186
Leandro Lima <lima44zero13@gmail.com>
2024-11-11 10:17:32 -0300

    Add recursos ao App

--- MauiAppHotel/Views/HospedagemContratada.xaml.cs
+++ MauiAppHotel/Views/HospedagemContratada.xaml.cs
@@ -6,5 +6,17 @@ namespace MauiAppHotel.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PopAsync();
+
+            }catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
     }
 }
